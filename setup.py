887f60175471f8ee084876004a837c693d328e2a
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-28 11:19:29 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyeventsummary',
-    version='0.0.1',
+    version='0.0.2',
     description='pyeventsummary is a way to aggregate and report on a host of errors and actions',
     long_description='pyeventsummary is a way to aggregate and report on a host of errors and actions',
     url='https://veltzer.github.io/pyeventsummary',
