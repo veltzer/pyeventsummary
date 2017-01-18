b8c228ef7db5145428b7c75b19aec9fd6fca4195
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-18 12:21:51 +0200

    add another many method

--- src/pyeventsummary/pyeventsummary.py
+++ src/pyeventsummary/pyeventsummary.py
@@ -1,7 +1,7 @@
 import enum
 from collections import defaultdict
-from typing import Iterable
-
+from typing import DefaultDict
+from typing import Iterable, Union
 
 """
 The errors class must be pickle ready since we may use it in multi-process, multi-thread or concurrent.futures
@@ -22,33 +22,37 @@ class EventSummary:
             enum_class: enum.Enum.__class__=None,
             enum_classes: Iterable[enum.Enum.__class__]=None,
             num_exceptions_saved: int=5,
-    ):  # type -> None
+    ) -> None:
         if enum_class is not None:
             self.enum_classes = [enum_class]  # type: Iterable[Iterable]
         if enum_classes is not None:
-            self.enum_classes = enum_classes
-        self.events = defaultdict(int)
-        self.num_exceptions_saved = num_exceptions_saved
+            self.enum_classes = enum_classes  # type: Iterable[Iterable]
+        self.events = defaultdict(int)  # type: DefaultDict[int]
+        self.num_exceptions_saved = num_exceptions_saved  # type: int
         self.exceptions_count = defaultdict(int)
         self.exceptions_saved = defaultdict(list)
 
-    def add_event(self, value):  # type -> None
+    def add_event(self, value) -> None:
         assert any(isinstance(value, cls) for cls in self.enum_classes), EventSummary.err_msg
         self.events[value] += 1
 
-    def get_event_count(self, value):  # type -> int
+    def get_event_count(self, value) -> int:
         assert any(isinstance(value, cls) for cls in self.enum_classes), EventSummary.err_msg
         return self.events[value]
 
-    def get_enum_classes(self):  # type -> list[class]
+    def get_enum_classes(self) -> Iterable[Iterable]:
         return self.enum_classes
 
-    def add(self, errors: 'EventSummary'):  # type -> None
+    def add(self, event_summary: 'EventSummary') -> None:
         for cls in self.enum_classes:  # type: Iterable
             for enum_member in cls:
-                self.events[enum_member] += errors.events[enum_member]
+                self.events[enum_member] += event_summary.events[enum_member]
 
-    def print(self, title=None):  # type -> None
+    def add_many(self, event_summaries: Iterable['EventSummary']) -> None:
+        for event_summary in event_summaries:
+            self.add(event_summary)
+
+    def print(self, title=None) -> None:
         if title:
             print(title)
         for cls in self.enum_classes:  # type: Iterable
@@ -60,7 +64,7 @@ class EventSummary:
     def __enter__(self):
         pass
 
-    def __exit__(self, e_type, e_val, trace_back):  # type -> Union[bool, None]
+    def __exit__(self, e_type, e_val, trace_back) -> Union[bool, None]:
         if e_type is not None:
             self.exceptions_count[e_type] += 1
             if len(self.exceptions_saved[e_type]) < self.num_exceptions_saved:
