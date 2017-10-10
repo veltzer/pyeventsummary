6b721f713b469d03ec6f0b103f6d47f4562d82f4
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-10 14:39:08 +0300

    setup.py filled

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
-
 setuptools.setup(
     name='pyeventsummary',
     version='0.0.15',
     description='pyeventsummary is a way to aggregate and report on a host of errors and actions',
     long_description='pyeventsummary is a way to aggregate and report on a host of errors and actions',
-    url='https://veltzer.github.io/pyeventsummary',
+    url='https://github.com/veltzer/pyeventsummary',
+    download_url='https://github.com/veltzer/pyeventsummary',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
     license='MIT',
+    platforms=['python'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
