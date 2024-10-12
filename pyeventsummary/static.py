59337b2e02a4f9ef1e2ee909804ccab0a0343406
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-12 14:53:17 +0300

    new version

--- pyeventsummary/static.py
+++ pyeventsummary/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.17"
+VERSION_STR = "0.0.18"
 DESCRIPTION = "pyeventsummary is a way to aggregate and report on a host of errors and actions"
 APP_NAME = "pyeventsummary"
 LOGGER_NAME = "pyeventsummary"
