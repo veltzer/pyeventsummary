413c8dc16cfe022fa7310643e791588513094a4d
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 11:36:24 +0300

    no commit message given

---
+++ config/project.lua
@@ -0,0 +1,11 @@
+NAME = "pyeventsummary"
+DESCRIPTION_SHORT = "pyeventsummary is a way to aggregate and report on a host of errors and actions"
+KEYWORDS = {
+    "python",
+    "summary",
+    "errors",
+    "actions",
+    "aggregate",
+    "print",
+    "enum",
+}
